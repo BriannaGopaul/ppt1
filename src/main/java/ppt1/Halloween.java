c245f475cf072bfe53673cec340129c4fb9dacaf
Sathish Gopalakrishnan <sathish.gopalakrishnan@gmail.com>
2018-10-10 07:08:38 -0700

    initialized PPT1

---
+++ src/main/java/ppt1/Halloween.java
@@ -0,0 +1,36 @@
+package ppt1;
+
+import java.util.List;
+
+public class Halloween {
+
+    /**
+     * Returns the total number of student meetings that happen when students go
+     * from a source to a destination. All sources are on the same street.
+     * All destinations are on the same street, but different from the source street.
+     * The source street and destination street are parallel to each other.
+     * @param students is not null
+     * @return the total number of student meetings that happen when students
+     *          go from a source to a destination.
+     */
+    public static int bottleCount(List<SDPair> students) {
+        // TODO: Implement this method
+        return -1;
+    }
+
+    /**
+     * Returns the index from the given list of the student that <strong>meets</strong> the maximum
+     * number of other students when students go from a source to a destination.
+     * All sources are on the same street. All destinations are on the same street,
+     * but different from the source street. The source street and destination
+     * street are parallel to each other.
+     * @param students is not null
+     * @return an index between 0 and students.size()-1 that represents the student
+     *          that meets most other students, an in case of ties the smaller index
+     *          is returned.
+     */
+    public static int highPoint(List<SDPair> students) {
+        // TODO: Implement this method
+        return -1;
+    }
+}
